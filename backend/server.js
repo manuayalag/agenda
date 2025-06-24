bdbb760c72478f9d36d6d6298e6d6bc561e8336e
Manuel Ayala <cesarayala@fiuni.edu.py>
2025-06-23 23:20:53 -0300

    cambios en app.listen ip 0.0.0.0 puerto 5000

--- backend/server.js
+++ backend/server.js
@@ -104,7 +104,7 @@ const startServer = async () => {
     console.log('Base de datos sincronizada correctamente');
     
     // Iniciar el servidor
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
   } catch (error) {
